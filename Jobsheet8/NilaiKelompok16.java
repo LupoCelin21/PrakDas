d773848b23bcde1f6f85b4e1d00a3c53376aa675
LupoCelin21 <emailanda@example.com>
2025-11-05 11:46:12 +0700

    Percobaan 4

---
+++ Jobsheet8/NilaiKelompok16.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+public class NilaiKelompok16 {
+public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    
+    int totalNilai ;
+    int nilai;
+    int ratanilai;
+
+    int keltertinggi = 0;
+    int ratatertinggi = 0;
+
+    int i = 1;
+        while (i <= 6){
+            System.out.println("kelompok " + i);
+
+            totalNilai = 0;
+            for (int j = 1; j<=5; j++){
+                System.out.print(" Nilai dari kelompok Penilai " + j + ": ");
+                nilai = sc.nextInt();
+                totalNilai += nilai;
+            }
+            ratanilai = totalNilai / 5;
+            if (ratanilai > ratatertinggi){
+                ratatertinggi = ratanilai;
+                keltertinggi = i;
+            }
+            System.out.print("kelompok " + i);
+            System.out.println(": nilai rata-rata = " + ratanilai);
+            i++;
+        }
+
+}
+}
